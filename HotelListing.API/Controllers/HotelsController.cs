80eacfcd95a5e8753d92260f1ef09e74c40f07fa
oğuzhan göley <oguzhanngoley@gmail.com>
2022-07-28 09:50:52 +0300

    Added HotelsController and HotelDtos

---
+++ HotelListing.API/Controllers/HotelsController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Hotel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly IHotelsRepository _hotelsRepository;
+        private readonly IMapper _mapper;
+
+        public HotelsController(IMapper mapper, IHotelsRepository hotelsRepository )
+        {
+            _hotelsRepository = hotelsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HotelDto>>> GetHotels()
+        {
+
+            var hotels = await _hotelsRepository.GetAllAsync();
+            return Ok(_mapper.Map<List<HotelDto>>(hotels));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HotelDto>> GetHotel(int id)
+        {
+            var hotel = await _hotelsRepository.GetAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            var hotelDto = _mapper.Map<HotelDto>(hotel);
+
+            return Ok(hotelDto);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHotel(int id, HotelDto updateHotelDto)
+        {
+            if (id != updateHotelDto.Id)
+            {
+                return BadRequest("Invalid Record Id");
+            }
+            var hotel = await _hotelsRepository.GetAsync(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(updateHotelDto, hotel);
+
+            try
+            {
+                await _hotelsRepository.UpdateAsync(hotel);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await HotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private async Task<bool> HotelExists(int id)
+        {
+            return await _hotelsRepository.Exists(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Hotel>> PostHotel(CreateHotelDto createHotel)
+        {
+            var hotel = _mapper.Map<Hotel>(createHotel);
+            await _hotelsRepository.AddAsync(hotel);
+            return CreatedAtAction("GetHotel", new { id = hotel.Id }, hotel);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            var country = await _hotelsRepository.GetAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            await _hotelsRepository.DeleteAsync(id);
+            return NoContent();
+        }
+    
+}
+}
